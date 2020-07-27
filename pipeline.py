01b45d3e25baed8ebfcf303358b58de8ec4f47e5
smg7d <smg7d@virginia.edu>
2020-07-26 22:53:17 -0400

    switch to using praw instead of roll your own authentication

--- pipeline.py
+++ pipeline.py
@@ -1,11 +1,21 @@
 import requests
 from authenticate import getToken
+import praw
+from secret import credentials
 
+user_agent = "canaryScanner by verykarmamuchreddit"
 
 def getData():
-    headers = {"Authorization": f"bearer {getToken()}", "User-Agent": "ChangeMeClient/0.1 by YourUsername"}
-    response = requests.get("https://oauth.reddit.com/api/v1/me", headers=headers)
-    print(response.json())
+    reddit = praw.Reddit(client_id=credentials["clientId"],
+    client_secret=credentials["clientSecret"],
+    user_agent=user_agent)
+
+    for submission in reddit.subreddit("learnpython").hot(limit=10):
+        print(submission.title)
+
+    # headers = {"Authorization": f"bearer {getToken()}", "User-Agent": "canaryScanner by verykarmamuchreddit"}
+    # response = requests.get("https://oauth.reddit.com/api/v1/me", headers=headers)
+    # print(response.json())
 
 
 if __name__ == "__main__":
